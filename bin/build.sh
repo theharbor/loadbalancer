b52d05e692b776d73cb07cb03246e4f6e26254a8
Nick Groenen <nick@groenen.me>
2014-12-08 08:18:11 +0100

    Enable localhost syslog udp listener

--- bin/build.sh
+++ bin/build.sh
@@ -8,4 +8,5 @@ alias aptinstall="apt-get install --no-install-recommends --yes"
 apt-get update
 apt-get dist-upgrade
 aptinstall wget daemontools haproxy  # daemontools for fghack
+sed -i -e 's/#source s_net {.*/source s_net { udp(ip(127.0.0.1) port(514)); };/' /etc/syslog-ng/syslog-ng.conf
 rm -rf /var/lib/apt/lists/*
